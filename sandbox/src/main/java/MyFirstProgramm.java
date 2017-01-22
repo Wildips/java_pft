a76acdef141e9f4884d4e2c11aad91c16ae12653
Wildips <zak125@mail.ru>
2017-01-22 15:25:51 +0300

    возврат наименования

--- sandbox/src/main/java/MyFirstProgramm.java
+++ sandbox/src/main/java/MyFirstProgramm.java
@@ -1,3 +1,4 @@
+public class MyFirstProgramm {
 	public static void main(String[] args){
 	System.out.println("Hello, world!");	
 	}
--- sandbox/src/main/java/MyFirstProgramm2.java
+++
@@ -1,5 +0,0 @@
-public class MyFirstProgramm2 {
-	public static void main(String[] args){
-	System.out.println("Hello, world!");	
-	}
-}
