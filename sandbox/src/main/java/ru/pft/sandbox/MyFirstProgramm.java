a5f4ea5a71b523210ad8ab8db486c9b19abfdc28
Wildips <zak125@mail.ru>
2017-01-22 16:18:20 +0300

    форматирование и стиль

--- sandbox/src/main/java/ru/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/pft/sandbox/MyFirstProgramm.java
@@ -1,7 +1,7 @@
 package ru.pft.sandbox;
 
 public class MyFirstProgramm {
-	public static void main(String[] args){
-	System.out.println("Hello, world!");	
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
