080e8f9cc2ecb9a38ad72425d549727f3efb1335
Wildips <zak125@mail.ru>
2017-03-30 23:37:03 +0300

    l3.3

--- adressbook-web-tests/src/test/java/ru/pft/adressbook/tests/TestBase.java
+++ adressbook-web-tests/src/test/java/ru/pft/adressbook/tests/TestBase.java
@@ -1,5 +1,6 @@
 package ru.pft.adressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.pft.adressbook.appmanager.ApplicationManager;
@@ -9,7 +10,7 @@ import ru.pft.adressbook.appmanager.ApplicationManager;
  */
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod
   public void setUp() throws Exception {
