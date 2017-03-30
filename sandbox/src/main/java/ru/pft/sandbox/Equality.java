bbeb1f806e1f19fc6de38faf16aba158cd14e160
Wildips <zak125@mail.ru>
2017-03-31 00:19:51 +0300

    l3.4

---
+++ sandbox/src/main/java/ru/pft/sandbox/Equality.java
@@ -0,0 +1,15 @@
+package ru.pft.sandbox;
+
+/**
+ * Created by Солнышко on 31.03.2017.
+ */
+public class Equality {
+
+  public static void main(String[] args){
+    String s1 = "firefox";
+    String s2 = new String(s1);
+
+    System.out.println(s1 == s2);
+    System.out.println(s1.equals(s2));
+  }
+}
