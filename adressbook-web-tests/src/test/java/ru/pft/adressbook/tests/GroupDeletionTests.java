d292160c8a17996313b91b7b6f0574fce09345d1
Wildips <zak125@mail.ru>
2017-04-06 00:01:09 +0300

    l3.10

--- adressbook-web-tests/src/test/java/ru/pft/adressbook/tests/GroupDeletionTests.java
+++ adressbook-web-tests/src/test/java/ru/pft/adressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.pft.adressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.pft.adressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
